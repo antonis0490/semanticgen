d165ad59b894da2df4d5e8d5b7b0895845078ed1
Antonis Charalambous <acharalampous@hotforex.com>
2016-10-22 12:36:40 +0300

    generator db config addition

--- generator/models.py
+++ generator/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class sentiments(models.Model):
+    id         = models.AutoField(primary_key=True)
+    url        = models.CharField()
+    sentiments = models.TextField()
+    date       = models.DateTimeField()
+
+    def __unicode__(self):
+        return self.url
